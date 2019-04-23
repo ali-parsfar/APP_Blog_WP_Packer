39aad46bc480b340b110a070c2d516bdcfbcbf8e
root <root@ausgs-docker01.dc01.fujixerox.net>
2019-04-23 21:56:28 +1000

    pwd changed , DB added

--- script.sh
+++ script.sh
@@ -22,7 +22,28 @@ sudo tar -xzvf latest.tar.gz
 sudo cp -r wordpress/* /var/www/html
 sudo rm -rf wordpress
 sudo rm -f latest.tar.gz
+sudo mv wp-config-sample.php wp-config.php
+sed -i -e 's/database_name_here/blog_wp_tst/g' p-config.php
+sed -i -e 's/username_here/blog/g' p-config.php
+sed -i -e 's/password_here/Bl0g_wp_tst/g' p-config.php
 sudo chown -R apache:apache /var/www/html/*
 sudo chmod 755 /var/www/html/*
 sudo systemctl  restart httpd
 
+#####
+# If /root/.my.cnf exists then it won't ask for root password
+cat <<-EOF >  /root/.my.cnf 
+[client]
+user=root
+password=P13@s3Ch@ng3m3
+EOF
+WPDB=blog_wp_tst
+WPUSR=blog
+WPPWD="Bl0g_wp_tst"
+
+sudo mysql -e "CREATE DATABASE ${WPDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+sudo mysql -e "CREATE USER ${WPUSR}@localhost IDENTIFIED BY '${WPPWD}';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON ${WPDB}.* TO '${WPUSR}'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+
+######
